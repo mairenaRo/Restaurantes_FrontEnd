e0e377a5546c7a9cd2fb5e10cf67225ff30a832d
Joel <107777818+EltitoCR@users.noreply.github.com>
2022-07-09 16:23:35 -0600

    Creacion de componentes
    Creación de componentes header, body y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+
+// Aquí vamos a importar los componentes para luego, este es el cerebro
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
